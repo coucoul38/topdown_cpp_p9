39a3087e8588d68dc3b353be4697bfa983e0987f
Noham Coulon <noham.coulon@gmail.com>
2025-01-31 11:56:55 +0100

    weaponComponent start

---
+++ Source/topdown_cpp/Private/WeaponComponent.cpp
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "WeaponComponent.h"
+
+// Sets default values
+AWeaponComponent::AWeaponComponent()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AWeaponComponent::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AWeaponComponent::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AWeaponComponent::Attack()
+{
+}
+
+float AWeaponComponent::GetSpeedModifier()
+{
+	return SpeedModifier;
+}
+
+float AWeaponComponent::GetAttackCooldown()
+{
+	return AttackCooldown;
+}
+
+float AWeaponComponent::GetDamage()
+{
+	return Damage;
+}
+
---
+++ Source/topdown_cpp/Public/WeaponComponent.h
@@ -0,0 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "WeaponComponent.generated.h"
+
+UCLASS()
+class TOPDOWN_CPP_API AWeaponComponent : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AWeaponComponent();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon")
+	FString WeaponName;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon")
+	float SpeedModifier;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon")
+	float AttackCooldown;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon")
+	float Damage;
+
+	UFUNCTION(BlueprintCallable, Category = "Weapon")
+	virtual void Attack();
+
+	UFUNCTION(BlueprintCallable, Category = "Weapon")
+	virtual float GetSpeedModifier();
+
+	UFUNCTION(BlueprintCallable, Category = "Weapon")
+	virtual float GetAttackCooldown();
+
+	UFUNCTION(BlueprintCallable, Category = "Weapon")
+	virtual float GetDamage();
+};
